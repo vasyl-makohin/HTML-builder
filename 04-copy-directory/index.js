36d777bba195a29f0bfe954f1cfe72954504d01b
Vasyl M <makohin.v@gmail.com>
2022-05-23 01:19:13 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,41 @@
+const fs = require("fs");
+const path = require("path");
 
+const oldFiles = path.join(__dirname, "files");
+const newFiles = path.join(__dirname, "files-copy");
+
+fs.mkdir(newFiles, { recursive: true }, (err) => {
+  if (err) {
+    throw err;
+  }
+
+  fs.readdir(newFiles, (err, files) => {
+    if (err) {
+      throw err;
+    }
+    files.forEach((file) => {
+      fs.unlink(path.join(newFiles, file), (err) => {
+        if (err) {
+          throw err;
+        }
+      });
+    });
+
+    fs.readdir(oldFiles, (err, files) => {
+      if (err) {
+        throw err;
+      }
+      files.forEach((file) => {
+        fs.copyFile(
+          path.join(oldFiles, file),
+          path.join(newFiles, file),
+          (err) => {
+            if (err) {
+              throw err;
+            }
+          }
+        );
+      });
+    });
+  });
+});
